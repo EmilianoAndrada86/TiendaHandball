024f1e99ebaeb071bfedc444d94f44826202ae06
Emiliano Andrada <emiamazon86@gmail.com>
2021-08-11 17:04:49 -0300

    Agregada pequena modificacion para desafio firebase

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -11,13 +11,7 @@ const GetITem=(key)=>{
             const db=getFirestore()
             const Itemcollection = db.collection("Items")
             Itemcollection.get().then((query)=>{query.docs.map(data=>{if(data.id==key){setItem(data.data())}} )})
-            const task  = new Promise((resolve,reject)=>{
-                setTimeout(()=>{
-                    resolve(articulos)
-                },2000)
-            })
-            
-            task.then((item)=>{item.articulos.map((it)=>{if(it.id==key) {setItem(it)} })})
+           
         })    
              return item
         }
